eba08d016cd340bb550153ac16d864f79ea3cdb6
keyplay <houyubo1993@foxmail.com>
2016-09-08 13:15:08 +0800

    Create initials.c

---
+++ harvard-CS50/pset2/initials.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include<ctype.h>
+
+int main(void)
+{
+    
+    string s = GetString();
+    
+    printf("%c", toupper(s[0]));
+    for(int i=1, n=strlen(s); i<n; i++)
+    {
+        if(s[i-1] == ' ')
+        {
+            printf("%c", toupper(s[i]));
+        }
+    }
+    printf("\n");
+    
+    return 0;
+}
