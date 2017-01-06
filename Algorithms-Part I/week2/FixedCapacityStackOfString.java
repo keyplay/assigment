7a29d72561016eabb91dc408543cf96cc04ce303
keyplay <houyubo1993@foxmail.com>
2017-01-06 15:22:01 +0800

    Create FixedCapacityStackOfString.java

---
+++ Algorithms-Part I/week2/FixedCapacityStackOfString.java
@@ -0,0 +1,20 @@
+public class FixedCapacityStackOfString {
+    private String[] s;
+    private int n = 0;
+    
+    public FixedCapacityStackOfString(int capacity) {
+        s = new String[capacity];
+    }
+    
+    public void push(String item) {
+        s[n++] = item;
+    }
+    
+    public String pop() {
+        return s[n--];
+    }
+    
+    public boolean isEmpty() {
+        return n == 0;
+    }
+}
