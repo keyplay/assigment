49ebc0b256d7aad0d961bdced0230bf89dd81b14
keyplay <houyubo1993@foxmail.com>
2017-01-06 14:52:08 +0800

    Create LinkedStackOfString.java

---
+++ Algorithms-Part I/week2/LinkedStackOfString.java
@@ -0,0 +1,25 @@
+public class LinkedStackOfString {
+    private Node first = null;
+    
+    private class Node {
+        String item;
+        Node next;
+    }
+    
+    public boolean isEmpty() {
+        return first == null;
+    }
+    
+    public void push(String s) {
+        Node oldfirst = first;
+        first = new Node();
+        first.item = s;
+        first.next = oldfirst;
+    }
+    
+    public String pop() {
+        String item = first.item;
+        first = first.next;
+        return item;
+    }
+}
